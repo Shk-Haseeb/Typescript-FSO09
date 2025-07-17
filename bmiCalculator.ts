530afa90b3ec18259f303782730199ed47c73ff2
Haseeb <haseeb.shaikh@helsinki.fi>
2025-07-17 21:53:28 +0300

    9.5: Added WebBMI endpoint

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,34 +1,9 @@
-const calculateBmi = (heightCm: number, weightKg: number): string => {
+export const calculateBmi = (heightCm: number, weightKg: number): string => {
   const heightInMeters = heightCm / 100;
-  const bmi = weightKg / (heightInMeters * heightInMeters);
+  const bmi = weightKg / (heightInMeters ** 2);
 
   if (bmi < 18.5) return 'Underweight';
   if (bmi < 25) return 'Normal range';
   if (bmi < 30) return 'Overweight';
   return 'Obese';
 };
-
-const parseBmiArguments = (args: string[]): { height: number; weight: number } => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
-
-  const height = Number(args[2]);
-  const weight = Number(args[3]);
-
-  if (isNaN(height) || isNaN(weight)) {
-    throw new Error('Provided values were not numbers!');
-  }
-
-  return { height, weight };
-};
-
-try {
-  const { height, weight } = parseBmiArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (e: unknown) {
-  let message = 'Something went wrong.';
-  if (e instanceof Error) message += ' Error: ' + e.message;
-  console.log(message);
-}
-
-//Added CLI interface for 9.3
